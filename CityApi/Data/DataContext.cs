90929513d7b171e8a567517c2009c652291018b7
Özcan ÇAVGA <81030695+OzcanCAVGA@users.noreply.github.com>
2024-07-10 21:42:42 +0300

    DataContext olusturuldu

---
+++ CityApi/Data/DataContext.cs
@@ -0,0 +1,15 @@
+﻿using CityApi.Core;
+using Microsoft.EntityFrameworkCore;
+
+namespace CityApi.Data
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base(options)
+        {
+
+        }
+        public DbSet<CityEntity> CityEntities{ get; set; }
+
+    }
+}
